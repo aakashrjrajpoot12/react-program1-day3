ca1fee9c2cd4879df3bb1c8379d74690ac8c047b
aakashrjrajpoot12 <aakashrjrajpoot@gmail.com>
2023-02-11 10:41:57 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -16,7 +16,31 @@ export class App2 extends React.Component {
       <div>
         <h1>Hello StackBlitz!</h1>
         <p>It is {new Date().toLocaleTimeString()}</p>
+    
       </div>
     );
   }
 }
+export function App3(props) {
+  
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>{props.name}</p>
+    </div>
+  );
+}
+export class App4 extends React.Component {
+  render(){
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>{this.props.city}</p>
+      <MyButton/>
+    </div>
+  );
+}
+}
+  function MyButton(){
+    return(<button>My Button</button>);
+  }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { App1, App2 } from './App';
+import { App1, App2,App3,App4,App5 } from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -10,5 +10,9 @@ root.render(
   <StrictMode>
     <App1 />
     <App2 />
+    <App3 name="Aakash"/>
+    <App4 city="banglore"/>
+ 
+
   </StrictMode>
 );

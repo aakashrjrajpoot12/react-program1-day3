3303e47351b482ea9fc4b88dfa6cc57f82e7482f
aakashrjrajpoot12 <aakashrjrajpoot@gmail.com>
2023-02-11 17:32:00 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css';
 //what is App >>It's react component
 export function App1() {
@@ -10,19 +10,17 @@ export function App1() {
   );
 }
 
-export class App2 extends React.Component {
+export class App2 extends React {
   render() {
     return (
       <div>
         <h1>Hello StackBlitz!</h1>
         <p>It is {new Date().toLocaleTimeString()}</p>
-    
       </div>
     );
   }
 }
 export function App3(props) {
-  
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
@@ -30,40 +28,37 @@ export function App3(props) {
     </div>
   );
 }
-export class App4 extends React.Component {
-  render(){
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>{this.props.city}</p>
-      <MyButton/>
-    </div>
-  );
-}
-}
-  function MyButton(){
-    return(<button>My Button</button>);
+export class App4 extends React {
+  render() {
+    return (
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>{this.props.city}</p>
+        <MyButton />
+      </div>
+    );
   }
+}
+function MyButton() {
+  return <button>My Button</button>;
+}
 
+export function App5(props) {
+  const emps = [
+    { name: 'aakash', id: 1 },
+    { name: 'sam', id: 2 },
+    { name: 'abhi', id: 3 },
+  ];
+  const elem = emps.map((emp) => (
+    <li key={emps.id}>
+      {emp.name}
+      {emp.id}
+    </li>
+  ));
+  console.log(typeof elem);
+  return <ul>{elem}</ul>;
 
-  export function App5(props) {
-
-    const emps=[
-      {name:"aakash",id:1},{name:"sam",id:2},{name:"abhi",id:3}
-      ];
-    const elem=emps.map(emp=>
-      <li  key={emps.id}>
-        {emp.name}
-        {emp.id}
-        </li>);
-        console.log(typeof elem)
-        return(
-        <ul>
-          {elem}
-          </ul>
-          );
-  
- /*
+  /*
     const names=[props.name];
    //const names=[...props.name]; 
    const elem=names.map(name=>
@@ -73,36 +68,42 @@ export class App4 extends React.Component {
         return(<ul>
           {elem}</ul>
           );
-    */    
+    */
+}
+export function App6(props) {
+  if (props.name != '') {
+    return <p>Hi,{props.name}</p>;
   }
-          export function App6(props) {
-           if(props.name!="")   {
-             return<p>Hi,{props.name}</p>;
+  return <p>Hi,Stranger</p>;
+}
+/*
+export default class App7 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date(), number: 10 };
+  }
+  changeState() {
+    this.setState({ date: new Date() });
+  }
 
-           }
-            return <p>Hi,Stranger</p>;
-        }
-      
+  render() {
+    this.changeState.number = 20;
 
-        export default class App7 extends React.Component {
-          constructor(props){
-          super(props);
-          this.state={date:new date(),number: 10};
-        }
-        changeState(){
-          this.setState({date:new Date()});
-
-        }
-          
-          
-          render(){
-            this.state.number=20;
-          return (
-            <div>
-              <h2>Clock</h2>
-              <p>It is {this.state.date.toLocaleTimeString()}now</p>
-              <p>{this.changeState()} </p>
-            </div>
-          );
-        }
-        }       
+    return (
+      <div>
+        <h2>Clock</h2>
+        <p>It is {this.state.date.toLocaleTimeString()}now</p>
+        <p>{this.changeState()} </p>
+        <MyButton1 data={this.state.date}/>
+      </div>
+    );
+  }
+}
+class MyButton1 extends Component{
+  render(){
+    return(
+      <input type="text" value={this.props.data}/>
+    );
+  }
+}
+*/
